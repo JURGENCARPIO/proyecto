81aa2db303eea4281b1363bedc32a8c1291cec23
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-16 09:21:33 -0500

    suma de numeros

---
+++ sumatoria de numeros/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,res:
+    cout<<"ingrese x: "<<endl:
+    cin>>x:
+    cout<<"ingrese y: "<< endl;
+    cin>>y:
+    res=x+y:
+    cout<<"el resultado es: "<<res<<endl:
+
+    return 0;
+}
