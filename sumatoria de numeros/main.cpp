edf9df4543ba72538facb1a650161605c880aa63
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 08:56:38 -0500

    modificaion

--- sumatoria de numeros/main.cpp
+++ sumatoria de numeros/main.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 int main()
 {
-    int x,y,res,z:
+    int x,y,res,z,a,b,c:
     cout<<"ingrese x: "<<endl:
     cin>>x:
     cout<<"ingrese y: "<< endl;
+
     cin>>y:
     res=x+y:
     cout<<"el resultado es: "<<res<<endl:
     cout<<"ingrese z: "<<endl:
     cin>>z:
     res=res+z:
-
+    cout<<"ingrese a: "<<endl:
+    cin>>a:
+    res=res+a:
+    cout<<"ingrese b: "<<endl:
+    cin>>b:
+    res=res+b:
+    cout<<"ingrese c: "<<endl:
+    cin>>c:
+    res=res+c:
     return 0;
 }
